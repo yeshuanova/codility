46d00097f0d123c595e5b1777a131efe906ac88f
CSL.Peter <yeshuanova@gmail.com>
2018-02-01 11:30:34 +0800

    Add solution: Lesson 7 - StoneWall

---
+++ Lessons/Lesson-7/StoneWall.cpp
@@ -0,0 +1,40 @@
+
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+int solution(vector<int> &A) {
+    auto container = vector<int>();
+    container.reserve(A.size());    // Avoid Segmentation Fault
+
+    auto stone_stack = stack<int, vector<int>>(container);
+
+    int stone_num = 0;
+    for (const auto& height : A) {
+
+        if (stone_stack.empty()) {
+            stone_stack.push(height);
+            stone_num += 1;
+            continue;
+        }
+
+        if (height > stone_stack.top()) {
+            stone_stack.push(height);
+            stone_num += 1;
+        } else {
+            while (!stone_stack.empty() && height < stone_stack.top()) {
+                stone_stack.pop();
+            }
+            if (height != stone_stack.top()) {
+                stone_stack.push(height);
+                stone_num += 1;
+            }
+        }
+    }
+    return stone_num;
+}
